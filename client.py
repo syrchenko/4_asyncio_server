77d7d67cd5183504ca1e0d3e4d6305e5a79ea89f
Арина Сырченко <syrchenkoarina@gmail.com>
2019-12-27 14:35:18 +0300

    commit1

--- client.py
+++ client.py
@@ -1,23 +1,50 @@
 import asyncio
 
+while True:
+    file=open('server.log','a')
+    HOST = input('Введите адрес хоста:\n')
+    file.write("Запрашиваю адрес хоста\n")
+    if HOST=='localhost':
+        HOST='127.0.0.1'
+        file.write(f"Введенный адрес хоста: {HOST} \n")
+        file.close()
+        break
+    if HOST=='':
+        file.write("Адрес хоста не установлен. \n")
+        file.close()
+        break
+    host_l=HOST.split('.')
+    if (0<=int(host_l[0])<255) and (0<=int(host_l[1])<255) and (0<=int(host_l[2])<255) and (0<=int(host_l[3])<255):
+        file.write(f"Введенный адрес хоста: {HOST} \n")
+        file.close()
+        break
+    else:
+        print('Введен неверный формат адреса.')
+        file.write("Введен неверный адрес хоста, запрашиваю адрес снова.\n")
 
-HOST = 'localhost'
-PORT = 9095
 
+while True:
+    file=open('server.log','a')
+    PORT=input('Введите номер порта от 1024 до 49151: \n')
+    PORT=int(PORT)
+    file.write("Запрашиваю номер порта\n")
+    if 1023<PORT<49152:
+        file.write(f"Введенный номер порта: {PORT} \n")
+        file.close()
+        break
+    else:
+        print('Неверный номер порта.')
+        file.write("Введен неверный номер порта, запрашиваю номер снова\n")
 
-async def tcp_echo_client(host, port):
-    reader, writer = await asyncio.open_connection(host, port)
-    message = 'Hello, world'
-
+async def tcp_echo_client(message, loop):
+    reader, writer = await asyncio.open_connection(HOST,PORT,loop=loop)
     writer.write(message.encode())
-    await writer.drain()
-
-    data = await reader.read(100)
+    data=await reader.read(100)
+    print(f'Отправлено: {data.decode()}')
+    print('Закрыл соединение')
     writer.close()
-    # await writer.wait_closed()
 
-# asyncio.run(tcp_echo_client(HOST, PORT))
-
-loop = asyncio.get_event_loop()
-task = loop.create_task(tcp_echo_client(HOST, PORT))
-loop.run_until_complete(task)
+message='Hello World!'
+loop=asyncio.get_event_loop()
+loop.run_until_complete(tcp_echo_client(message,loop))
+loop.close()
--- server.py
+++ server.py
@@ -1,18 +1,48 @@
 import asyncio
 
-HOST = 'localhost'
-PORT = 9095
+while True:
+    file=open('server.log','a')
+    HOST = input('Введите адрес хоста:\n')
+    file.write("Запрашиваю адрес хоста\n")
+    if HOST=='localhost':
+        HOST='127.0.0.1'
+        file.write(f"Введенный адрес хоста: {HOST} \n")
+        file.close()
+        break
+    if HOST=='':
+        file.write("Адрес хоста не установлен. \n")
+        file.close()
+        break
+    host_l=HOST.split('.')
+    if (0<=int(host_l[0])<255) and (0<=int(host_l[1])<255) and (0<=int(host_l[2])<255) and (0<=int(host_l[3])<255):
+        file.write(f"Введенный адрес хоста: {HOST} \n")
+        file.close()
+        break
+    else:
+        print('Введен неверный формат адреса.')
+        file.write("Введен неверный адрес хоста, запрашиваю адрес снова.\n")
+
+
+while True:
+    file=open('server.log','a')
+    PORT=input('Введите номер порта от 1024 до 49151: \n')
+    PORT=int(PORT)
+    file.write("Запрашиваю номер порта...\n")
+    if 1023<PORT<49152:
+        file.write(f"Введенный номер порта: {PORT} \n")
+        file.close()
+        break
+    else:
+        print('Неверный номер порта.')
+        file.write("Введен неверный номер порта, запрашиваю номер снова...\n")
 
 
 async def handle_echo(reader, writer):
     data = await reader.read(100)
-    message = data.decode()
-
     writer.write(data)
     await writer.drain()
 
     writer.close()
-
 
 loop = asyncio.get_event_loop()
 coro = asyncio.start_server(handle_echo, HOST, PORT, loop=loop)
